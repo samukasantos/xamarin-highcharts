27786328cb8087bf8e48a8560d0ff9ae5d6b5207
dionisoliveira <dionisoliveira@gmail.com>
2015-04-13 23:27:03 -0300

    Inclusão do serviço.

--- Xamarin.HighCharts/Xamarin.HighCharts/App.cs
+++ Xamarin.HighCharts/Xamarin.HighCharts/App.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
+
+using Xamarin.HighCharts.Services.WCFHighChartsService;
 
 namespace Xamarin.HighCharts
 {
+
     public class App
     {
+
+
         public static Page GetMainPage()
         {
+              WCFHighChartsServiceClient webService = new WCFHighChartsServiceClient();
+         
+            User user = new User();
+            user.ID = 1;
+            user.Login = "highcharts";
+            user.Password = "123";
+            user.Name = "charts";
+         
+            webService.AddUserAsync(user);
+           
             return new ContentPage
             {
                 Content = new Label
@@ -20,6 +35,14 @@ namespace Xamarin.HighCharts
                     HorizontalOptions = LayoutOptions.CenterAndExpand,
                 },
             };
+
+            webService.DeleteUserAsync(user.ID);
         }
+
+      
+
+
+
+
     }
 }
